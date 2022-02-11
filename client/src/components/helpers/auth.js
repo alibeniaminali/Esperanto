065f5b8113e3287d59f8d5404fdf9f52b0f4451e
Peter Bid <peterbid21@hotmail.com>
2022-02-11 17:48:31 +0000

    Basic Frontend Framework

---
+++ client/src/components/helpers/auth.js
@@ -0,0 +1,22 @@
+export const getTokenFromLocalStorage = () => {
+  return window.localStorage.getItem('teachers-token')
+}
+
+
+export const getPayload = () => {
+  const token = getTokenFromLocalStorage() 
+  console.log('I am the token', token)
+  if (!token) return
+  const splitToken = token.split('.')
+  if (splitToken.length !== 3) return
+  console.log(splitToken[1])
+  return JSON.parse(Buffer.from(splitToken[1], 'base64'))
+}
+
+
+export const userIsAuthenticated = () => {
+  const payload = getPayload()
+  if (!payload) return
+  const currentTime = Math.round(Date.now() / 1000)
+  return currentTime < payload.exp
+}
---
+++ client/src/components/teachers/UserProfile.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const UserProfile = () => {
+
+  return (
+    <h2>This is User Profile Page</h2>
+  )
+}
+
+export default UserProfile
