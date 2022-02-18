0784843b5388820f410a2f8b05d10b9da01f5052
Dougie <dougie-b@hotmail.com>
2022-02-18 10:42:17 +0000

    so close

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -28,7 +28,6 @@ const Home = () => {
   useEffect(() => {
     function countLanguages() {
       let arr = []
-      console.log('it\'s me')
       teachers.forEach(teachers => {
         if (!arr.includes(teachers.teaches)) {
           arr.push(teachers.teaches)
@@ -37,12 +36,11 @@ const Home = () => {
       setLanguages(arr)
     }
     countLanguages()
-  })
+  }, [teachers])
 
   return (
     <>
       {/* Row 1 */}
-
       <Container fluid>
         <Row className="row1">
           <Col sm={5}>
@@ -52,16 +50,13 @@ const Home = () => {
               Learn a language in the early morning, lunchtime, evenings or even at weekends. Choose from Arabic, French, German, Greek, Italian and a whole bunch more! One to one and group classes also available.
             </p>
             <p>Choose your language and expand your horizons! 🌎</p>
-
             <Link to="/teachers" className='btn btn-warning'>Find your teacher ↩️ </Link>
           </Col>
           <Col sm={3}>
             <img src={LearningLady} className="img-thumbnail" alt="animated teacher"></img>
           </Col>
         </Row>
-
         {/* End of Row 1 */}
-
         {/* Row 2 */}
         <Row className="row2">
           <Col sm={3}>
@@ -76,9 +71,7 @@ const Home = () => {
             </ul>
           </Col>
         </Row>
-
         {/* End of Row 2 */}
-
         <Row className="row3">
           <Col sm={4}>
             <h3>Learn more than just a language</h3>
@@ -91,58 +84,3 @@ const Home = () => {
 }
 
 export default Home
-
-
-// {/* <Row className="row2">
-//     <Col sm>
-//       <Card>
-//         <Card.Body>
-//           <Card.Title>Large selection of languages </Card.Title>
-//           <Card.Subtitle className="mb-2 text-muted">
-//             Online Learning
-//           </Card.Subtitle>
-//           <Card.Text>
-//             Find the languages you want to learn and practice at your own pace.
-//           </Card.Text>
-//         </Card.Body>
-//       </Card>
-//     </Col>
-
-//   <Col sm>
-//     <Card>
-//       <Card.Body>
-//         <Card.Title>Native-speaking teachers</Card.Title>
-//         <Card.Subtitle className="mb-2 text-muted">
-//           Live online sessions with your teachers
-//         </Card.Subtitle>
-//         <Card.Text>
-//           Find teachers from all over the world sharing their languages,
-//           dialects, and cultures.
-//         </Card.Text>
-//       </Card.Body>
-//     </Card>
-//   </Col>
-
-//   <Col sm>
-//   <Card>
-//     <Card.Body>
-//       <Card.Title>Lifetime access </Card.Title>
-//       <Card.Subtitle className="mb-2 text-muted">
-//         Online learning
-//       </Card.Subtitle>
-//       <Card.Text>
-//         We keep your course materials so you can access them whenever you need.
-//       </Card.Text>
-//     </Card.Body>
-//   </Card>
-//   </Col>
-//   </Row> */}
-
-
-// {/* <Col sm={4}>
-//       <img
-//       src="https://media.istockphoto.com/photos/student-using-laptop-having-online-class-with-teacher-picture-id1288092444?b=1&k=20&m=1288092444&s=170667a&w=0&h=L968NhkwgRZhpHWoNtdKy_Hk4kNELzb88Jp9Ja5qzWY="
-//       className="img-thumbnail"
-//       alt="person studying online">
-//       </img>
-//     </Col> */}
