0e88aa5cb6ef606b4d8f22782040114a9f7fb2f6
Peter Bid <peterbid21@hotmail.com>
2022-02-21 13:06:01 +0000

    heroku stuff

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import express from 'express'
 import mongoose from 'mongoose'
 import { port, dbURI } from './config/environment.js'
 import router from './config/routes.js'
+import 'dotenv/config' // only needs to be added if it doesn't already exist
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const app = express()
 
@@ -17,8 +23,14 @@ const startServer = async () => {
 
     app.use('/api', router)
 
+    app.use(express.static(path.join(__dirname, 'client', 'build')))
+
     app.use((req, res) => {
       return res.status(404).json({ message: 'failed to connect' })
+    })
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
 
     app.listen(port, () => console.log(`Lovely jubbly, port ${port}`))
